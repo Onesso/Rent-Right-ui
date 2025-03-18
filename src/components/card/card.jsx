34e58e0d7e2578a435198d06509666d6ae52b15b
Onesso <odhiambofrank1965@gmail.com>
2025-03-18 11:11:44 +0300

    scrowlable images and filter done

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -8,7 +8,40 @@ export default function Card({ item }) {
       <Link to={`/${item}`} className="imageContainer">
         <img src={item.images} alt="property image" />
       </Link>
-      <div className="textContainer"></div>
+
+      <div className="textContainer">
+        <h2 className="title">
+          <Link to={`/${item}`}>{item.title}</Link>
+        </h2>
+        <p className="address">
+          <img src="./pin.png" alt="" />
+          <span>{item.address}</span>
+        </p>
+        <p className="price">KSH {item.price}</p>
+
+        <div className="buttom">
+          <div className="features">
+            <div className="feature">
+              <img src="./bed.png" alt="bed icon" />
+              <span>{item.bedroom} bedroom</span>
+            </div>
+
+            <div className="feature">
+              <img src="./bath.png" alt="bed icon" />
+              <span>{item.bathroom} bathroom</span>
+            </div>
+          </div>
+
+          <div className="icons">
+            <div className="icon">
+              <img src="./save.png" alt="save icon" />
+            </div>
+            <div className="icon">
+              <img src="./chat.png" alt="save icon" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
